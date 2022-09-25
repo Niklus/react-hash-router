c036c8ecadae52411edd2be0fd88adb95aeb1bd2
Niklus <niklasoti@gmail.com>
2022-09-25 08:00:55 +0200

    Update

--- src/App.js
+++ src/App.js
@@ -1,36 +1,32 @@
-import React, { useState, useEffect } from "react";
-import Home from "./components/Home";
-import About from "./components/About";
-import Contact from "./components/Contact";
-import Header from "./components/Header";
+import React, { useState, useEffect } from 'react';
+import Home from './components/Home';
+import About from './components/About';
+import Contact from './components/Contact';
+import Header from './components/Header';
 
-import "./style.css";
+import './style.css';
 
 function App() {
   const [hash, setHash] = useState(window.location.hash);
 
-  const handleHashChange = () => {
-    setHash(window.location.hash);
-  };
+  useEffect(() => {
+    window.addEventListener('hashchange', () => {
+      setHash(window.location.hash);
+    });
+  }, []);
 
-  useEffect(()=>{
-    window.addEventListener("hashchange", handleHashChange);
-  },[]); // empty array means run only once to avoid adding multiple listeners hence no memory leaks
-
-  const routes = {
-    "": <Home />,
-    "#home": <Home />,
-    "#about": <About />,
-    "#contact": <Contact />,
+  const router = {
+    '#home': <Home />,
+    '#about': <About />,
+    '#contact': <Contact />,
   };
 
   return (
     <>
       <Header />
-      {routes[hash]}
+      {router[hash] ?? router['#home']}
     </>
   );
 }
 
 export default App;
-
